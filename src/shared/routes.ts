37437b57a9b79063e4b74669ea5561bd15b1aa89
dileepk706 <dlpkmr706@gmail.com>
2026-04-07 02:31:28 +0530

    feat(api): set up query client and shared routes

---
+++ src/shared/routes.ts
@@ -0,0 +1,51 @@
+const memberRoot = `/members`;
+const gymRoot = `/gym`;
+const accountRoot = `/account`;
+
+export const pathKeys = {
+  root: '/',
+  login: '/login/',
+  register: '/register/',
+  page404: '/404/',
+
+  // dashboard
+  dashboard: {
+    root: '/',
+  },
+
+  // gym
+  gym: {
+    root: gymRoot,
+    schedule: `${gymRoot}/schedule/`,
+    staffs: {
+      root: `${gymRoot}/staffs/`,
+      byStaffId: (id: string) => `${gymRoot}/staffs/${id}/`,
+    },
+  },
+
+  // members
+  members: {
+    root: memberRoot,
+    checkIn: `${memberRoot}/check-in/`,
+    byMemberId: (id: string) => `${memberRoot}/${id}/`,
+  },
+
+  // account
+  account: {
+    root: accountRoot,
+    setting: `${accountRoot}/setting/`,
+  },
+
+  // auth
+  auth: {
+    memberLogin: '/:gymId/member/login/',
+    staffLogin: '/:gymId/staff/login/',
+    userLogin: '/login/',
+    userRegister: '/register/',
+  },
+};
+
+// export const userPermissionsMap = {
+export const userPermissionsMap = {};
+
+export const paths = pathKeys;
